298d286f18c22c702ffe253aa1e6a2e552445305
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-14 04:17:08 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/login-and-register-using-react/",
   plugins: [react()],
   test: {
     globals: true,
