434de9f545b7ad7d23586b10b8d204e58318d41f
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-14 04:24:17 +0300

    add: deploy workflow

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer className="flex">
-
+      
       <p  className="max">© 2024 by Mohammed salem khred. All rights reserved.</p>
       <p className="mini"> © 2024 by Mohammed salem khred</p>
     </footer>
